8920555bc4157a5c9fdced387fc2571dc34e9255
harrynguyen0312 <little.ruan@gmail.com>
2019-10-01 00:00:05 +0700

    - init

---
+++ demo-authentication-web/src/main/java/org/hikarikyou/demo/main/Application.java
@@ -0,0 +1,13 @@
+package org.hikarikyou.demo.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
+
+@SpringBootApplication
+@ImportResource({"classpath:spring-cfg.xml"})
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
