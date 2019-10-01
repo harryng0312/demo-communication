ad7df54d8022025dd3d0eba7638eb355cde9698a
harrynguyen0312 <little.ruan@gmail.com>
2019-10-02 01:53:12 +0700

    - init

--- demo-authentication-web/src/main/java/org/harryng/demo/controller/AuthController.java
+++ demo-authentication-web/src/main/java/org/harryng/demo/controller/AuthController.java
@@ -2,11 +2,22 @@ package org.harryng.demo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AuthController {
-    @RequestMapping("/login")
-    public String login() {
-        return "login";
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String initLogin() {
+        return "auth/login";
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public String submitLogin() {
+        String rs = "auth/login";
+        boolean result = false;
+        if (result) {
+            rs = String.format("redirect:%s", "user/welcome");
+        }
+        return rs;
     }
 }
