d45a534f13e8d31b45919d5d6fedb7dd0021d4bc
Harry Nguyen <harry@videabiz.com>
2019-10-01 15:34:35 +0700

    - Init

---
+++ demo-authentication-web/src/main/java/org/harryng/demo/controller/HelloController.java
@@ -0,0 +1,14 @@
+package org.harryng.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(@RequestParam(name = "name", required = false, defaultValue = "World") String name) {
+        return "Hello " + name;
+    }
+
+}
