cf8bbc5986ace9df3329b99126f10841663fbafd
Harry Nguyen <harry@videabiz.com>
2019-10-02 11:36:34 +0700

    - Init

--- demo-authentication-web/src/main/java/org/harryng/demo/main/Application.java
+++ demo-authentication-web/src/main/java/org/harryng/demo/main/Application.java
@@ -2,11 +2,17 @@ package org.harryng.demo.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ImportResource({"classpath:spring-cfg.xml"})
-public class Application {
+public class Application extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
