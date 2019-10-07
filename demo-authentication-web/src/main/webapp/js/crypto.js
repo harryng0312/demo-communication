6f329e52aa89f5c9580eba51a2c541064e0bb22a
Harry Nguyen <harry@videabiz.com>
2019-10-07 16:37:20 +0700

    - use JPA Hibernate

--- demo-authentication-web/src/main/webapp/js/crypto.js
+++ demo-authentication-web/src/main/webapp/js/crypto.js
@@ -1,40 +1,41 @@
 var HCrypto = {
-    subtle: window.crypto.subtle?window.crypto.subtle:widow.msCrypto.subtle,
+    subtle: window.crypto.subtle ? window.crypto.subtle : widow.msCrypto.subtle,
     // SHA-256
-    hash: async function (algName, str) {
-        var promise = this.subtle.digest({name: algName}, strToArrBuffer(str))
-            .then(function (value) {
-                return DataUtil.bytesToBase64(value);
-            })
-        var rs = await promise;
+    hash: function (algName, str) {
+        var promise = this.subtle.digest({name: algName}, DataUtil.strToBytes(str));
+        promise.then(function (value) {
+            return DataUtil.bytesToBase64(value);
+        });
+        var rs = promise;
         return rs;
     },
     // HMAC RSA-PSS ECDSA
-    sign: async function (algName, key, str) {
+    sign: function (param, key, str) {
         var keyBin = DataUtil.base64ToBytes(key);
-        var promise = this.subtle.sign({name: algName}, keyBin, DataUtil.strToBytes(str))
-            .then(function (value) {
-                return DataUtil.bytesToBase64(value);
-            }).catch(function(err) {
-                console.error(err);
-            })
-        var rs = await promise;
+        var promise = this.subtle.sign(param, keyBin, DataUtil.strToBytes(str));
+        promise.then(function (value) {
+            return DataUtil.bytesToBase64(value);
+        }).catch(function (err) {
+            console.error(err);
+        });
+        var rs = promise;
         return rs;
     },
-    verify: async function(algName, key, str){
+    verify: function (algName, key, str) {
         var keyBin = DataUtil.base64ToBytes(key);
-        var promise = this.subtle.verify({name: algName}, keyBin, DataUtil.strToBytes(str))
-            .then(function (value) {
-                return DataUtil.bytesToBase64(value);
-            }).catch(function(err) {
-                console.error(err);
-            })
-        var rs = await promise;
+        var promise = this.subtle.verify(param, keyBin, DataUtil.strToBytes(str));
+        promise.then(function (value) {
+            return DataUtil.bytesToBase64(value);
+        }).catch(function (err) {
+            console.error(err);
+        });
+        var rs = promise;
         return rs;
     },
 
     //
-    generateKey: async function(algName, ){
-        this.subtle
+    generateKey: function (param, keyUsages) {
+        var keyPair = this.subtle.generateKey(param, true, keyUsages);
+        return keyPair;
     },
 }
