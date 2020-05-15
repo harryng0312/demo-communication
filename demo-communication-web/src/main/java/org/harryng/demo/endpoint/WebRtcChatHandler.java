dcf1baa3f87eb12153d3fdbfb9dbaafafa0de44b
Harry Nguyen <harry@videabiz.com>
2020-05-15 11:42:07 +0700

    - disable default security - login/logout by form - csrf - multi login - custom authentication provider by username password

--- demo-communication-web/src/main/java/org/harryng/demo/endpoint/WebRtcChatHandler.java
+++ demo-communication-web/src/main/java/org/harryng/demo/endpoint/WebRtcChatHandler.java
@@ -1,6 +1,5 @@
 package org.harryng.demo.endpoint;
 
-import org.harryng.demo.controller.WebRTCController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.socket.TextMessage;
