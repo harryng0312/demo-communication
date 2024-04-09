367295ca912eeab1b25a8fe4883340a01c0ae50d
harryng0312 <little.ruan@gmail.com>
2024-04-09 20:30:59 +0700

    infinispan

--- demo-communication-web/src/test/java/org/harryng/demo/cache/TestCache.java
+++ demo-communication-web/src/test/java/org/harryng/demo/cache/TestCache.java
@@ -4,12 +4,11 @@ import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.harryng.demo.main.Application;
 import org.infinispan.Cache;
-import org.infinispan.configuration.cache.CacheMode;
-import org.infinispan.configuration.cache.ConfigurationBuilder;
-import org.infinispan.configuration.global.GlobalConfigurationBuilder;
-import org.infinispan.configuration.parsing.ConfigurationBuilderHolder;
 import org.infinispan.manager.DefaultCacheManager;
-import org.infinispan.remoting.transport.jgroups.JGroupsTransport;
+import org.infinispan.manager.EmbeddedCacheManager;
+import org.infinispan.util.concurrent.locks.KeyAwareLockPromise;
+import org.infinispan.util.concurrent.locks.LockManager;
+import org.infinispan.util.concurrent.locks.impl.InfinispanLock;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -22,7 +21,7 @@ import java.util.concurrent.TimeUnit;
 @Slf4j
 public class TestCache {
     @Resource
-    private DefaultCacheManager cacheManager;
+    private EmbeddedCacheManager cacheManager;
 
     @BeforeAll
     public static void init() {
@@ -43,11 +42,29 @@ public class TestCache {
     }
 
     @Test
-    public void testCache() {
+    public void testCacheLock() {
+        // do not need to lock by manual as below
         log.info("=====");
         final Cache<String, String> cache = cacheManager.getCache("dist-cache");
-        for (String cacheName: cacheManager.getCacheNames()){
-            log.info("cacheName:{}", cacheName);
+
+        final LockManager lockManager = cache.getAdvancedCache().getLockManager();
+        final String key = "k1";
+        final String owner = "owner 1";
+        cache.put(key, "v1", 1000, TimeUnit.MINUTES);
+        final KeyAwareLockPromise lock = lockManager.lock(key, owner, 10, TimeUnit.SECONDS);
+        try {
+            lock.lock();
+            log.info("LockOwner:{}", lockManager.getOwner(key));
+            log.info("Lock status:{} {}", lockManager.getLock(key).isLocked(), lockManager.isLocked(key));
+            for (String cacheName : cacheManager.getCacheNames()) {
+                log.info("cacheName:{}", cacheName);
+            }
+        } catch (InterruptedException e) {
+            log.error("", e);
+        } finally {
+            lockManager.unlock(key, owner);
+
         }
+        log.info("Lock status after unlock:{}", lockManager.isLocked(key));
     }
 }
