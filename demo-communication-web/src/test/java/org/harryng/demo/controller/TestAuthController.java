7083883e72d028523e643885e70438d9832fca6f
harryng0312 <little.ruan@gmail.com>
2024-04-27 00:17:16 +0700

    Test AuthController

--- demo-communication-web/src/test/java/org/harryng/demo/controller/TestAuthController.java
+++ demo-communication-web/src/test/java/org/harryng/demo/controller/TestAuthController.java
@@ -18,7 +18,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest(classes = Application.class,
-        webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Import(Application.class)
 @AutoConfigureMockMvc
 @AutoConfigureWebTestClient
