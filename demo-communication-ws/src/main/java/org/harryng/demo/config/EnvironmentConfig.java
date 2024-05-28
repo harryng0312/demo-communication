a68e9251b4ad3f030c068247edb328f045d22263
harryng0312 <little.ruan@gmail.com>
2024-05-28 16:30:12 +0700

    deploy ws by sts

--- demo-communication-ws/src/main/java/org/harryng/demo/config/EnvironmentConfig.java
+++ demo-communication-ws/src/main/java/org/harryng/demo/config/EnvironmentConfig.java
@@ -6,7 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 
-@Configuration
+//@Configuration
 @Slf4j
 public class EnvironmentConfig {
     @Resource
