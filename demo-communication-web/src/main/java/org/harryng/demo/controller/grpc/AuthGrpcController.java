f43facf87f595b66f0e092b995498d2c4f2fd0cb
harryng0312 <little.ruan@gmail.com>
2024-05-30 00:40:56 +0700

    gRPC

--- demo-communication-web/src/main/java/org/harryng/demo/controller/grpc/AuthGrpcController.java
+++ demo-communication-web/src/main/java/org/harryng/demo/controller/grpc/AuthGrpcController.java
@@ -1,24 +1,23 @@
 package org.harryng.demo.controller.grpc;
 
 import io.grpc.stub.StreamObserver;
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import net.devh.boot.grpc.server.service.GrpcService;
 import org.harryng.demo.api.util.ResponseWrapper;
-import org.harryng.demo.api.util.TextUtil;
-import org.harryng.demo.controller.grpc.auth.dto.LoginReq;
-import org.harryng.demo.controller.grpc.auth.dto.LoginRes;
+import org.harryng.demo.controller.grpc.auth.AuthControllerGrpc;
+import org.harryng.demo.controller.grpc.auth.LoginReq;
+import org.harryng.demo.controller.grpc.auth.LoginRes;
 import org.harryng.demo.impl.auth.dto.AuthenticationInfo;
 import org.harryng.demo.impl.auth.service.AuthService;
 import org.harryng.demo.impl.user.service.UserService;
 import org.springframework.stereotype.Component;
 
-@Component("authGrpcController")
+@Component
 @GrpcService
 @RequiredArgsConstructor
 @Slf4j
-public class AuthController extends AuthControllerGrpc.AuthControllerImplBase {
+public class AuthGrpcController extends AuthControllerGrpc.AuthControllerImplBase {
 
     protected final AuthService authService;
     protected final UserService userService;
--- demo-communication-web/src/main/java/org/harryng/demo/controller/grpc/HelloWorldController.java
+++ demo-communication-web/src/main/java/org/harryng/demo/controller/grpc/HelloWorldController.java
@@ -1,38 +1,76 @@
-package org.harryng.demo.controller.grpc;
-
-import io.grpc.stub.StreamObserver;
-import lombok.extern.slf4j.Slf4j;
-import net.devh.boot.grpc.server.service.GrpcService;
-import org.springframework.stereotype.Component;
-
-import java.text.MessageFormat;
-
-@Component
-@GrpcService
-@Slf4j
-public class HelloWorldController extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
-    @Override
-    public StreamObserver<HelloWorldRequest> sayHello(StreamObserver<HelloWorldResponse> responseObserver) {
-        log.info("+++++ Connected +++++");
-        return new StreamObserver<>() {
-            @Override
-            public void onNext(HelloWorldRequest helloWorldRequest) {
-                final var name = helloWorldRequest.getName();
-                log.info("from request:{}", name);
-                final var item = HelloWorldResponse.newBuilder()
-                        .setGreeting(MessageFormat.format("Hello {0} from server", name))
-                        .build();
-                responseObserver.onNext(item);
-            }
-            @Override
-            public void onError(Throwable throwable) {
-                log.error(throwable.getMessage(), throwable);
-            }
-            @Override
-            public void onCompleted() {
-                responseObserver.onCompleted();
-                log.info("----- Disconnected -----");
-            }
-        };
-    }
-}
+//package org.harryng.demo.controller.grpc;
+//
+//import io.grpc.stub.StreamObserver;
+//import lombok.extern.slf4j.Slf4j;
+//import net.devh.boot.grpc.server.service.GrpcService;
+//import org.springframework.stereotype.Component;
+//
+//import java.text.MessageFormat;
+//
+//@Component
+//@GrpcService
+//@Slf4j
+//public class HelloWorldController extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
+//    @Override
+//    public StreamObserver<HelloWorldRequest> sayHello(StreamObserver<HelloWorldResponse> responseObserver) {
+//        log.info("+++++ Connected +++++");
+//        return new StreamObserver<>() {
+//            @Override
+//            public void onNext(HelloWorldRequest helloWorldRequest) {
+//                final var name = helloWorldRequest.getName();
+//                log.info("from request:{}", name);
+//                final var item = HelloWorldResponse.newBuilder()
+//                        .setGreeting(MessageFormat.format("Hello {0} from server", name))
+//                        .build();
+//                responseObserver.onNext(item);
+//            }
+//            @Override
+//            public void onError(Throwable throwable) {
+//                log.error(throwable.getMessage(), throwable);
+//            }
+//            @Override
+//            public void onCompleted() {
+//                responseObserver.onCompleted();
+//                log.info("----- Disconnected -----");
+//            }
+//        };
+//    }
+//}
+//package org.harryng.demo.controller.grpc;
+//
+//import io.grpc.stub.StreamObserver;
+//import lombok.extern.slf4j.Slf4j;
+//import net.devh.boot.grpc.server.service.GrpcService;
+//import org.springframework.stereotype.Component;
+//
+//import java.text.MessageFormat;
+//
+//@Component
+//@GrpcService
+//@Slf4j
+//public class HelloWorldController extends HelloWorldServiceGrpc.HelloWorldServiceImplBase {
+//    @Override
+//    public StreamObserver<HelloWorldRequest> sayHello(StreamObserver<HelloWorldResponse> responseObserver) {
+//        log.info("+++++ Connected +++++");
+//        return new StreamObserver<>() {
+//            @Override
+//            public void onNext(HelloWorldRequest helloWorldRequest) {
+//                final var name = helloWorldRequest.getName();
+//                log.info("from request:{}", name);
+//                final var item = HelloWorldResponse.newBuilder()
+//                        .setGreeting(MessageFormat.format("Hello {0} from server", name))
+//                        .build();
+//                responseObserver.onNext(item);
+//            }
+//            @Override
+//            public void onError(Throwable throwable) {
+//                log.error(throwable.getMessage(), throwable);
+//            }
+//            @Override
+//            public void onCompleted() {
+//                responseObserver.onCompleted();
+//                log.info("----- Disconnected -----");
+//            }
+//        };
+//    }
+//}
