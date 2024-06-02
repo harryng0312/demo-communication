cabca2219843e48c42047b1c32bde7e554bc78f2
harryng0312 <little.ruan@gmail.com>
2024-06-03 01:12:45 +0700

    call to local external pgsql

--- demo-communication-web/src/main/java/org/harryng/demo/aop/LoggingAspect.java
+++ demo-communication-web/src/main/java/org/harryng/demo/aop/LoggingAspect.java
@@ -5,6 +5,7 @@ import org.aspectj.lang.ProceedingJoinPoint;
 
 import java.time.LocalDateTime;
 import java.time.temporal.ChronoUnit;
+import java.util.UUID;
 
 @Slf4j
 public class LoggingAspect {
@@ -16,13 +17,16 @@ public class LoggingAspect {
         log.info("-----");
     }
 
-    public Object around(ProceedingJoinPoint jp) throws Throwable{
+    public Object around(ProceedingJoinPoint jp) throws Throwable {
+        final UUID uuid = UUID.randomUUID();
         final LocalDateTime start = LocalDateTime.now();
-        log.info("+++++ [REQUEST]:{}.{}({}) +++++", jp.getTarget().getClass().getSimpleName(), jp.getSignature().getName(), jp.getArgs());
-        final Object result = jp.proceed();
-        final LocalDateTime finish = LocalDateTime.now();
-        log.info("----- [RESPONSE]:{}.{} ----- {} millisecond(s)", jp.getTarget().getClass().getSimpleName(), jp.getSignature().getName(),
-                start.until(finish, ChronoUnit.MILLIS));
-        return result;
+        log.info("+++++ [REQUEST][{}]:{}.{}({}) +++++", uuid, jp.getTarget().getClass().getSimpleName(), jp.getSignature().getName(), jp.getArgs());
+        try {
+            return jp.proceed();
+        } finally {
+            final LocalDateTime finish = LocalDateTime.now();
+            log.info("----- [RESPONSE][{}]: {}.{} ----- {} millisecond(s)", uuid, jp.getTarget().getClass().getSimpleName(), jp.getSignature().getName(),
+                    start.until(finish, ChronoUnit.MILLIS));
+        }
     }
 }
