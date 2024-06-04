e0a43228e69a30bb0b28ed93dbdd5a945fc89bd9
harryng0312 <little.ruan@gmail.com>
2024-06-04 21:23:23 +0700

    split to configuration map

--- demo-communication-web/src/main/java/org/harryng/demo/config/PersistenceConfig.java
+++ demo-communication-web/src/main/java/org/harryng/demo/config/PersistenceConfig.java
@@ -2,8 +2,11 @@ package org.harryng.demo.config;
 
 import jakarta.persistence.ValidationMode;
 import org.harryng.demo.impl.base.persistence.SimpleBasePersistence;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -25,19 +28,23 @@ import javax.sql.DataSource;
 )
 public class PersistenceConfig {
 
+    private static final Logger log = LoggerFactory.getLogger(PersistenceConfig.class);
+
+    @Value("${spring.datasource.url}")
+    private String dsUrl;
+
     @Primary
     @Bean(name = "entityManagerFactory")
     public LocalContainerEntityManagerFactoryBean primaryEntityManager(@Autowired DataSource dataSource) {
+        log.info("db_url:{}", dsUrl);
         final LocalContainerEntityManagerFactoryBean em = new LocalContainerEntityManagerFactoryBean();
         em.setDataSource(dataSource);
         em.setPersistenceUnitName("primary");
         em.setValidationMode(ValidationMode.NONE);
         em.setPackagesToScan("org.harryng.demo.impl.**.entity", "org.harryng.demo.impl.**.persistence");
-//
         final HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
         em.setJpaVendorAdapter(vendorAdapter);
 //        em.setJpaPropertyMap(hibernateProperties());
-
         return em;
     }
 
